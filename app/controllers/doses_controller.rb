4a7544e4812a9695beeac2cde5db128fc3781218
lise henry <lise.henry@essca.eu>
2021-02-25 17:21:46 +0100

    added more routes

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,36 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail), notice: 'Dose was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    # set_dose
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+end
